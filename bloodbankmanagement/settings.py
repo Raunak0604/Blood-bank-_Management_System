2e20d94070709c09662dd76cbd7ba60cb4748f7d
Raunak0604 <raunakmondal0604@gmail.com>
2022-08-21 00:20:02 +0530

    File added

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 # STATIC_DIR=os.path.join(BASE_DIR,'static')
-STATIC_ROOT=os.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
