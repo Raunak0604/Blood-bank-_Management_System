f70efc62439b58e93226c7992a5a6004ab1919b4
Raunak0604 <raunakmondal0604@gmail.com>
2022-08-21 02:06:54 +0530

    File added

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['iblood.herokuapp.com', '127.0.0.1',]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.blood_stock',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
